434b9fcad0d085d1803ea016199831e6cedabd12
Sawyer Shepherd <contact@sawyershepherd.org>
2022-03-29 11:46:04 -0400

    Add regex match to list

--- list.rb
+++ list.rb
@@ -1,3 +1,20 @@
 def list
-  puts Dir.children(HOME_TRASH_FILES_DIRECTORY)
+
+  if $args.length == 0
+    # Given no args, show every trashed file
+    puts Dir.children(HOME_TRASH_FILES_DIRECTORY)
+
+  elsif $args.length == 1
+    # Given a regex pattern as an arg, print trashed files
+    # that fit
+    regex = Regexp.new($args[0])
+    puts Dir.children(HOME_TRASH_FILES_DIRECTORY).select { |file|
+      regex =~ file
+    }
+
+  else
+    raise StandardError.new(
+      "can: mode --list expects 0 to 1 arguments, given #{$args.length}"
+    )
+  end
 end
