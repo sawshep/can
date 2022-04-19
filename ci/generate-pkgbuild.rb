0f8fff689581299cf355471ad846ac2c3280e70f
Sawyer Shepherd <contact@sawyershepherd.org>
2022-04-19 18:15:38 -0400

    Fix bumping pkgrel on version bump

--- ci/generate-pkgbuild.rb
+++ ci/generate-pkgbuild.rb
@@ -5,33 +5,55 @@ require 'json'
 require 'net/http'
 require 'uri'
 
+GEM_NAME = 'can_cli'
+PKG_NAME = 'can'
+
 ci_dir = File.expand_path(File.dirname(__FILE__))
 repo_root = File.expand_path(File.join(ci_dir, '..'))
 
-require_relative File.join(repo_root, 'lib/can/version')
+require_relative File.join(repo_root, 'lib/#{PKG_NAME}/version')
+
+VERSION = Can::VERSION
 
 # Get the sha256sum from RubyGems
-gems_api_uri = URI.parse("https://rubygems.org/api/v1/gems/can_cli.json")
+gems_api_uri = URI.parse("https://rubygems.org/api/v1/gems/#{GEM_NAME}.json")
 gems_api_response = Net::HTTP.get_response(gems_api_uri)
 sha256sum = JSON.parse(gems_api_response.body)["sha"]
 
 # Get the old PKGBUILD from the AUR to find the old $pkgrel
-old_pkgbuild_uri = URI.parse("https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=can")
+old_pkgbuild_uri = URI.parse("https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=#{PKG_NAME}")
 pkgbuild_response = Net::HTTP.get_response(old_pkgbuild_uri)
-pkgrel_regex = /^pkgrel=([0-9]+?)$/
-old_pkgrel = pkgrel_regex.match(pkgbuild_response.body)[1]
-new_pkgrel = old_pkgrel.to_i + 1
+
+version_regex = /^pkgver=(.+?)$/
+old_version = if pkgbuild_response.code == '404'
+                nil
+              else
+                version_regex.match(pkgbuild_response.body)[1]
+              end
+
+pkgrel_regex = /^pkgrel=(\d+?)$/
+old_pkgrel = if pkgbuild_response.code == '404'
+               0
+             else
+               pkgrel_regex.match(pkgbuild_response.body)[1]
+             end
+
+new_pkgrel = if VERSION == old_version
+               old_pkgrel.to_i + 1
+             else
+               1
+             end
 
 pkgbuild = <<~PKGBUILD
   # Maintainer: Sawyer Shepherd <contact@sawyershepherd.org>
 
-  _gemname=can_cli
-  pkgname=can
-  pkgver=#{Can::VERSION}
+  _gemname=#{GEM_NAME}
+  pkgname=#{PKG_NAME}
+  pkgver=#{VERSION}
   pkgrel=#{new_pkgrel}
   pkgdesc='Command-line trash manager'
   arch=(any)
-  url='https://github.com/sawshep/can'
+  url='https://github.com/sawshep/#{PKG_NAME}'
   license=(GPL-3.0)
   depends=(ruby ruby-highline)
   options=(!emptydirs)
